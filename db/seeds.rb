af7c463cef460405a2fd0ef88ea31430f63bb015
Romain Morlevat <romainmorlevat@gmail.com>
2019-08-26 15:19:02 +0200

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+%w[general react Lyon].each do |name|
+  Channel.create(name: name)
+end
+
+user1 = User.create(email: 'romainmorlevat@gmail.test', password: 123_456)
+user2 = User.create(email: 'johndoe@gmail.test', password: 654_321)
+
+Message.create(content: 'First message.', user: user1, channel: Channel.first)
+Message.create(content: 'Second message.', user: user2, channel: Channel.first)
