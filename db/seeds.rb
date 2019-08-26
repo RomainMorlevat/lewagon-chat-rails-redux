768cf4d0bc559a453bfd3ef2102f1ca94d398f54
Romain Morlevat <romainmorlevat@gmail.com>
2019-08-26 15:21:41 +0200

    Improve seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+puts('Destroy Messages, Channels and Users')
+Message.destroy_all
+Channel.destroy_all
+User.destroy_all
+
+puts('Create fake data')
 %w[general react Lyon].each do |name|
-  Channel.create(name: name)
+  Channel.create!(name: name)
 end
 
-user1 = User.create(email: 'romainmorlevat@gmail.test', password: 123_456)
-user2 = User.create(email: 'johndoe@gmail.test', password: 654_321)
+user1 = User.create!(email: 'romainmorlevat@gmail.test', password: 123_456)
+user2 = User.create!(email: 'johndoe@gmail.test', password: 654_321)
 
-Message.create(content: 'First message.', user: user1, channel: Channel.first)
-Message.create(content: 'Second message.', user: user2, channel: Channel.first)
+Message.create!(content: 'First message.', user: user1, channel: Channel.first)
+Message.create!(content: 'Second message.', user: user2, channel: Channel.first)
